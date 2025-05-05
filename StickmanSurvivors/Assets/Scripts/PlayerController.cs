64311ac04dc6ab0cc4e731b1872280dfbf4220c2
Aleksander Szymura <minecreolo@gmail.com>
2025-05-05 14:02:47 +0200

    New CameraFollow script. Expansive tilemap. Random map generation using tilemap.

--- StickmanSurvivors/Assets/Scripts/PlayerController.cs
+++ StickmanSurvivors/Assets/Scripts/PlayerController.cs
@@ -1,30 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
     [Header("Ruch gracza")]
+    [Tooltip("Prêdkoœæ poruszania siê gracza w jednostkach na sekundê")]
     public float speed = 5f;
+    [Tooltip("Rigidbody2D gracza")]
     public Rigidbody2D myRigidbody;
+
+    private Vector2 input;
+    private Vector2 movement;
 
     void Awake()
     {
+        // Pobranie komponentu, jeœli nie zosta³ ustawiony w Inspektorze
         if (myRigidbody == null)
             myRigidbody = GetComponent<Rigidbody2D>();
+
+        // Ustawienia fizyki dla top-downa
+        myRigidbody.gravityScale = 0f;
+        myRigidbody.freezeRotation = true;
+        myRigidbody.interpolation = RigidbodyInterpolation2D.Interpolate;
+        myRigidbody.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
     }
 
     void Update()
     {
-        float moveX = Input.GetAxis("Horizontal");
-        float moveY = Input.GetAxis("Vertical");
-        movement = new Vector2(moveX, moveY);
+        // Wejœcie bez wyg³adzania – od razu -1, 0 lub +1
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
     }
-
-    private Vector2 movement;
 
     void FixedUpdate()
     {
-        Vector2 newPos = myRigidbody.position + movement * speed * Time.fixedDeltaTime;
-        myRigidbody.MovePosition(newPos);
+        // Znormalizowane do sta³ej prêdkoœci w dowolnym kierunku
+        movement = input.normalized * speed;
+        // Ustawiamy prêdkoœæ – silnik fizyki zadba o kolizje
+        myRigidbody.velocity = movement;
     }
 }
